46c5117a514bf381d115bb7db69116f600ab749c
Paul Shorey <pshorey@beyond.ai>
2019-02-27 14:12:54 -0800

    git add .js files

---
+++ test/sort-by-preferences.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const tape_1 = __importDefault(require("tape"));
+const sort_by_preferences_1 = __importDefault(require("../lib/sort-by-preferences"));
+const sort = sort_by_preferences_1.default.bind(null, ["index.md", "README.md"]);
+tape_1.default("sorts with normal string sort", t => {
+    t.deepEqual([4, 2, 3, 1].sort(sort), [1, 2, 3, 4]);
+    t.deepEqual(["a", "C", "A", "B", "~", "ab", "b"].sort(sort), [
+        "A",
+        "B",
+        "C",
+        "a",
+        "ab",
+        "b",
+        "~"
+    ]);
+    t.end();
+});
+tape_1.default("sorts but puts first preferent strings", t => {
+    t.deepEqual(["a", "C", "README.md", "A", "index.md", "B", "~", "ab", "b"].sort(sort), ["README.md", "index.md", "A", "B", "C", "a", "ab", "b", "~"]);
+    t.end();
+});
