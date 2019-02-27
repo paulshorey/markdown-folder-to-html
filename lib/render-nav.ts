f1f6c0e31a63789b0600d265247b1111f2e61aea
Paul Shorey <pshorey@beyond.ai>
2019-02-27 11:09:12 -0800

    fix typescript types in render-nav

--- lib/render-nav.ts
+++ lib/render-nav.ts
@@ -1,50 +1,75 @@
-import { FileTree, IndexFile, FileFolder, File } from "./types";
-import assertNever from "./assert-never";
+"use strict";
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//   return (mod && mod.__esModule) ? mod : {
+//     "default": mod
+//   };
+// };
+// Object.defineProperty(exports, "__esModule", {
+//   value: true
+// });
+// const assert_never_1 = __importDefault(require("./assert-never"));
 
-export default function renderNav(
-  groupedFiles: FileTree<IndexFile>,
-  level = 0
-): string {
-  return `<ul>
-${groupedFiles
-    .map(f => {
-      if (f.type === "dir") {
-        const childrenNav = renderNav(f.children, level + 1);
-        const indexFile = getIndexFile(f.children);
-        // Heading with link if there is an index file in the folder
-        if (indexFile) {
-          const link = renderActive(
-            f.name,
-            indexFile.value.href,
-            indexFile.value.active
-          );
-          return `<li class="heading">${link}</li>\n${childrenNav}`;
-        }
-        // Heading without link
-        return `<li class="heading"><span>${
-          f.name
-        }</span></li>\n${childrenNav}`;
-      } else if (f.type === "file") {
-        //  Leaf
-        const { text, href, active } = f.value;
-
-        // Skip index files on nested levels since the Heading links to them.
-        if (level > 0 && text && text.toLowerCase() === "index") return;
-
-        return `<li>${renderActive(text, href, active)}</li>`;
+export default function renderNav(groupedFiles: any, level = 0) {
+  // Build <li>s
+  var nav = '';
+  groupedFiles.forEach((f: any) => {
+    /*
+        DIR
+    */
+    if (f.type === "dir") {
+      const childrenNav = renderNav(f.children, level + 1);
+      // Heading with link if there is an index file in the folder
+      const indexFile = getIndexFile(f.children);
+      if (indexFile) {
+        const link = renderHREF(f.name, indexFile.value.href, indexFile.value.active);
+        nav += `<li class="heading">${link}</li>\n${childrenNav}`;
+        return;
       }
-      return assertNever(f);
-    })
-    .join("\n")}
-</ul>`;
+      // Heading without link
+      nav += `<li class="heading"><span>${f.name}</span></li>\n${childrenNav}`;
+      return;
+    }
+    /*
+        FILE
+    */
+    if (f.type === "file") {
+      const {
+        text,
+        href,
+        active
+      } = f.value;
+      // Skip index files on nested levels since the Heading links to them.
+      // if (level > 0 && text && (text.toLowerCase() === "index" || text.toLowerCase() === "readme")) {
+      // 	return;
+      // }
+      // Render link
+      nav += `<li>${renderHREF(text, href, active)}</li>`;
+      return;
+    }
+    /*
+        N/A
+    */
+    // return assert_never_1.default(f);
+  });
+  return `<ul>${nav}</ul>`;
 }
-
-function renderActive(text: string, href: string, active: boolean) {
-  return `<a class="${active ? "active" : ""}" href="${href}">${text}</a>`;
+exports.default = renderNav;
+/*
+    <a href=...
+*/
+function renderHREF(text: any, href: any, active: any) {
+  if (text == "README" || text == "index") {
+    return '';
+  }
+  return `<a class="${active ? "active" : ""}" href="./${href}">${text}</a>`;
 }
-
-function getIndexFile(files: FileTree<IndexFile>): File<IndexFile> | undefined {
-  return files.find(e => e.type === "file" && e.value.text === "index") as File<
-    IndexFile
-  >; // Stupid TS
+/*
+    index.html or README.html
+*/
+function getIndexFile(files: any) {
+  var indexFile = files.find((e: any) => e.type === "file" && e.value.text === "index");
+  if (!indexFile) {
+    indexFile = files.find((e: any) => e.type === "file" && e.value.text === "README");
+  };
+  return indexFile;
 }
